d1b8f1cae05d16fdebb5369915fcd53fdd20882e
Jobayer98 <joobayerrahman0220@gmail.com>
2024-01-18 17:02:56 +0600

    find middle node from LinkList

--- LinkedList/linkedList.py
+++ LinkedList/linkedList.py
@@ -1,3 +1,4 @@
+import math
 class Node:
     def __init__(self, data):
         self.data = data
@@ -8,6 +9,9 @@ class LinkList:
         self.head = Node(data)
         self.tail = self.head
         self.length = 1
+        
+    def __str__(self):
+        return f"{self.head.data}"
         
         
     def append(self, data):
@@ -45,14 +49,28 @@ class LinkList:
                 dec_v += pow(2, i)
                 
         return dec_v
+    
+    def middleNode(self):
+        length = 0
+        current_pointer = self.head
+        while current_pointer:
+            length += 1
+            current_pointer = current_pointer.next
         
-        
+        current_pointer = self.head
+        for i in range(math.floor(length/2)):
+            current_pointer = current_pointer.next
+
+        self.head = current_pointer 
+    
 def main():
     myList = LinkList(1)
-    myList.append(0)
-    myList.append(1)
+    myList.append(2)
+    myList.append(3)
+    myList.append(4)
+    myList.append(5)
+    myList.middleNode()
     print(myList.printList())
-    myList.getDecimalValue()
 
 if __name__== '__main__':
     main()
