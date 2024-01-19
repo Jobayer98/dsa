77669b9b9f9bfa13b4bda1a8ff8461a8858d57d4
Jobayer98 <joobayerrahman0220@gmail.com>
2024-01-19 15:34:57 +0600

    trees

---
+++ Tree/binary_s_tree.js
@@ -0,0 +1,63 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      return (this.root = newNode);
+    }
+
+    let currentNode = this.root;
+    while (true) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  lookup(value) {
+    let currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.value === value) return currentNode;
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return false;
+  }
+
+  remove(value) {}
+}
+
+const obj = new BinarySearchTree();
+obj.insert(9);
+obj.insert(4);
+obj.insert(21);
+obj.insert(6);
+obj.insert(1);
+obj.insert(15);
+obj.insert(170);
+
+console.log(obj.lookup(1));
---
+++ Tree/binary_tree.py
@@ -0,0 +1,19 @@
+class Node :
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self) -> None:
+        self.root = None
+        
+    def insert(data):
+        pass
+    
+    def lookup(data):
+        pass
+    
+
+obj = BinarySearchTree()
